a1b5972b45fa930fbe8213913adee53f1308be9a
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2023-09-14 17:25:45 +0530

    added file

--- devops/runner-update/runner-info-and-update.sh
+++ devops/runner-update/runner-info-and-update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 yaml_file="$GITHUB_WORKSPACE/kustomization.yml"
 echo $yaml_file
 
@@ -9,7 +10,7 @@ echo "latest_release=$latest_release"
 
 latest_release_date=$(curl -s https://api.github.com/repos/actions/runner/releases/latest | jq -r '.published_at')
 echo "latest_release_date=$latest_release_date" >> $GITHUB_ENV
-echo "latest_release_date=$latest_release_date"
+# echo "latest_release_date=$latest_release_date"
 
 previous_major_version=$(echo $latest_release | awk -F'.' '{print $1"."$2-1}')
 echo "previous_major_version=$previous_major_version" >>$GITHUB_ENV
@@ -25,15 +26,19 @@ echo "latest_release_previous_major=$latest_release_previous_major"
 echo "latest_release_previous_major=$latest_release_previous_major" >>$GITHUB_ENV
 
 latest_release_previous_major_date=$(echo "$releases_previous_major" | jq -r '.[0].published_at')
-echo "latest_release_previous_major_date=$latest_release_previous_major_date"
+# echo "latest_release_previous_major_date=$latest_release_previous_major_date"
 echo "latest_release_previous_major_date=$latest_release_previous_major_date" >> $GITHUB_ENV
 
 CURRENT_VERSION=$(grep -A 1 'name: summerwind/actions-runner' './kustomization.yml' | grep 'newTag' | awk -F 'newTag:' '{print $2}' | sed 's/^[ \t]//;s/[ \t]$//')
 echo "CURRENT_VERSION=$CURRENT_VERSION"
 echo "CURRENT_VERSION=$CURRENT_VERSION" >>$GITHUB_ENV
 
+extracted_version=$(echo "$CURRENT_VERSION" | cut -d'-' -f1)
+echo "extracted_version: ${extracted_version}"
+
 latest_release_date_formatted=$(date -d "$latest_release_date" '+%Y-%m-%d')
 echo "latest_release_date_formatted: $latest_release_date_formatted"
+
 latest_release_previous_major_date_formatted=$(date -d "$latest_release_previous_major_date" '+%Y-%m-%d')
 echo "latest_release_previous_major_date_formatted: $latest_release_previous_major_date_formatted"
 
@@ -41,17 +46,22 @@ date_diff=$(( ($(date -d "$latest_release_date" '+%s') - $(date -d "$latest_rele
 
 echo "date_diff=$date_diff"
 
+expected_date_diff=30
+
 if [ "$latest_release_previous_major" != "" ]; then
   echo "Latest release from the previous major version: $latest_release_previous_major"
-
-  if [ "$latest_release_previous_major" != "$CURRENT_VERSION" ]; then
-    extracted_version=$(echo "$CURRENT_VERSION" | cut -d'-' -f1)
-    echo "extracted_version: ${extracted_version}"
-    new_tag_value="${latest_release_previous_major}"
+  
+  if [ "$latest_release_previous_major" != "$extracted_version" ]; then
+    if [ "$date_diff" -ge "$expected_date_diff" ]; then
+      echo "Upgrade timeframe is over for previous major version. Upgradding to latest version"
+      new_tag_value="${latest_release}"
+    else
+      new_tag_value="${latest_release_previous_major}"
+    fi
     echo "new_tag_value: ${new_tag_value}"
+    echo "new_tag_value=$new_tag_value" >>$GITHUB_ENV
     sed -i "s/\(newTag: \)$extracted_version/\1$new_tag_value/g" "$yaml_file"
     echo "::set-output name=notify::true"
-
     # echo "release_data='Current Version: $CURRENT_VERSION, Latest Release: $latest_release, Previous Major Version: $previous_major_version, Latest Release from Previous Major: $latest_release_previous_major'" >> $GITHUB_ENV
   else
     echo "No new releases available"

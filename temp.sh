0a4619803f7f63195515e3eeeec61a310965c0bc
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2024-01-08 17:01:30 +0530

    testing POC for [deploy: name1 name2 name3 name4 name5] auto deploy PR

--- temp.sh
+++ temp.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+# #!/bin/bash
 
-COMMIT_MESSAGE="hi im ajinkya [deploy: name1 name2 name3 name4 name5] do it"
-tenant_names=$(echo "$COMMIT_MESSAGE" | sed -n 's/.*\[deploy:[[:space:]]*\([^]]*\)].*/\1/p')
-echo "Extracted Tenant Names: $tenant_names"
+# COMMIT_MESSAGE="hi im ajinkya [deploy: name1 name2 name3 name4 name5] do it"
+# tenant_names=$(echo "$COMMIT_MESSAGE" | sed -n 's/.*\[deploy:[[:space:]]*\([^]]*\)].*/\1/p')
+# echo "Extracted Tenant Names: $tenant_names"
+
+TAG="1629119469-170f8167-car----14792-[cust{om@@e#$@rio-us...er-i:nvit____easdfhkjfhiuhdf___"
+TAG=$(echo "${TAG}" | sed -E  -e 's/-+/-/g' -e 's/_+/_/g' -e 's/.+/./g' -e 's/[^a-zA-Z0-9._-]//g'  -e 's/[._-]*$//g')
+TAG=${TAG:0:63}
+# echo "your_string_here" | sed -n '/^[a-zA-Z0-9._-]//g*[a-zA-Z0-9]*$//g'
+
+echo "TAG=$TAG"

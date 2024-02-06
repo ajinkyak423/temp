1362f462a5378b19867e8a8c4d8f858cd9159e21
ajinkyak423 <ajinkyakumbhar423@gmail.com>
2024-02-06 10:41:46 +0530

    error

--- temp.py
+++ temp.py
@@ -1,40 +1,45 @@
-import re
-from jinja2 import Template
-import logging
-import sys
+# import re
+# from jinja2 import Template
+# import logging
+# import sys
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s   %(name)s   %(levelname)s  %(message)s')
-logger = logging.getLogger('init-container')
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s   %(name)s   %(levelname)s  %(message)s')
+# logger = logging.getLogger('init-container')
 
-templateFile = "secrets.yaml"
+# templateFile = "secrets.yaml"
 
-with open(templateFile, 'r') as f:
-    data = f.read()
+# with open(templateFile, 'r') as f:
+#     data = f.read()
 
-secretDict = {}
+# secretDict = {}
 
-# regex pattern to match the secret names
-pattern = r'secret\["([^"]+)"\]'
+# # regex pattern to match the secret names
+# pattern = r'secret\["([^"]+)"\]'
 
-# find all matches in the given data
-secret_names = set(re.findall(pattern, data))
-logger.info("Secrets found in input file: %s", secret_names)
+# # find all matches in the given data
+# secret_names = set(re.findall(pattern, data))
+# logger.info("Secrets found in input file: %s", secret_names)
 
-# secrets = []
-# for secret in secret_names:
-#     if secret not in secrets:
-#         secrets.append(secret)
-#         print("secret: \n", secret )
+# # secrets = []
+# # for secret in secret_names:
+# #     if secret not in secrets:
+# #         secrets.append(secret)
+# #         print("secret: \n", secret )
 
-print("secrets:", secret_names)
-print(len(secret_names))
+# print("secrets:", secret_names)
+# print(len(secret_names))
 
-template = Template(open(templateFile).read())
-try:
-    rendered = template.render(secret=secretDict)
-except Exception as e:
-    logger.error("Error rendering template: %s", e)
-    sys.exit(1)
+# template = Template(open(templateFile).read())
+# try:
+#     rendered = template.render(secret=secretDict)
+# except Exception as e:
+#     logger.error("Error rendering template: %s", e)
+#     sys.exit(1)
 
-
-hi
+ERROR_MSGS = [] 
+ERROR_MSGS.append("This is test error")
+if True:
+        for err_msg in globals.ERROR_MSGS:
+            print(f"::error::{err_msg}")
+        print("Lint checks failed.")
+        exit(1)
